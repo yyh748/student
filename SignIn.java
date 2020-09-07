e6e0738255ea5cf75c7782a21bcc7274c43f1d3a
yyh <1544071073@qq.com>
2020-09-07 17:02:03 +0800

    在家继续开发签到功能

---
+++ SignIn.java
@@ -0,0 +1,11 @@
+public class SignIn{
+	private int id;
+	private String name;
+	public void sign(){
+		System.out.println("在家开发代码");
+	}
+	public void sign(int id){
+				System.out.println("在家开发代码");
+
+	}
+ }
